5c29a171c60af4467721564979433c64389d11ea
abxglia <abhiprajapati@lampros.tech>
2025-08-01 09:57:58 +0000

    first commit

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { ICounter } from "./ICounter";
 import { ethers } from "ethers";
 
-const contractAddress = "0x664508a7178451da209b9387f671c347eb28c0cc";
+const contractAddress = "0x4d299fc99a08b6073a196a67f2e75fa8678d9258";
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
